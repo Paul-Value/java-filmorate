7ae12ab7123c46b7cd497808a1c1dbc3458e5619
Paul Value <paulvalue@gmail.com>
2024-05-01 19:23:57 +0300

    Initial commit

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -1,7 +1,31 @@
 package ru.yandex.practicum.filmorate.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import ru.yandex.practicum.filmorate.model.Film;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @RestController
+@RequestMapping("/films")
 public class FilmController {
+    private final Map<Integer, Film> films = new HashMap<>();
+
+    @GetMapping
+    public ArrayList<Film> getAllFilms() {
+        return new ArrayList<>(films.values());
+    }
+
+    private long getNextId() {
+        long currentMaxId = films.keySet()
+                .stream()
+                .mapToInt(id -> id)
+                .max()
+                .orElse(0);
+        return ++currentMaxId;
+    }
 }
--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -1,12 +1,22 @@
 package ru.yandex.practicum.filmorate.model;
 
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import lombok.Setter;
+
+import java.time.Duration;
+import java.time.LocalDate;
 
 /**
  * Film.
  */
 @Getter
 @Setter
+@RequiredArgsConstructor
 public class Film {
+    private int id;
+    private String name;
+    private String description;
+    private LocalDate releaseDate;
+    private Duration duration;
 }
