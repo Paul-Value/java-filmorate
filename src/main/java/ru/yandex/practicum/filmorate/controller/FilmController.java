bb7495bd9958c05687604d5fb9f907fd699b0b6f
Paul Value <paulvalue@gmail.com>
2024-06-05 20:14:28 +0300

    11 ТЗ: Исправление замечаний ревью: перенос логирования в сервисы

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -3,7 +3,6 @@ package ru.yandex.practicum.filmorate.controller;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Positive;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.model.Film;
@@ -13,7 +12,6 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/films")
-@Slf4j
 @RequiredArgsConstructor
 public class FilmController {
     private final FilmService filmService;
@@ -21,46 +19,32 @@ public class FilmController {
 
     @GetMapping
     public List<Film> getAll() {
-        log.info("GET /films");
         return filmService.getAll();
     }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public Film create(@Valid @RequestBody Film film) {
-        log.info("==>POST /films {}", film);
-        Film newFilm = filmService.save(film);
-        log.info("POST /films <== {}", newFilm);
-        return newFilm;
+        return filmService.save(film);
     }
 
     @PutMapping
     public Film update(@Valid @RequestBody Film film) {
-        log.info("PUT /films {}", film);
-        Film updatedFilm = filmService.update(film);
-        log.info("PUT /films <== {}", updatedFilm);
-        return updatedFilm;
+        return filmService.update(film);
     }
 
     @PutMapping("/{id}/like/{userId}")
     public void like(@PathVariable long id, @PathVariable long userId) {
-        log.info("PUT /films/{}/like/{} add like", id, userId);
         filmService.addLike(id, userId);
-        log.info("PUT /films/{}/like/{} like added", id, userId);
     }
 
     @DeleteMapping("/{id}/like/{userId}")
     public void deleteLike(@PathVariable long id, @PathVariable long userId) {
-        log.info("DELETE /films/{}/like/{} delete", id, userId);
         filmService.deleteLike(id, userId);
-        log.info("DELETE /films/{}/like/{} deleted", id, userId);
     }
 
     @GetMapping("/popular")
     public List<Film> getPopular(@RequestParam(defaultValue = "10") @Positive int count) {
-        log.info("GET /films/popular?count={} get list of popular films", count);
-        List<Film> films = filmService.getPopular(count);
-        log.info("GET /films/popular?count={} list of popular films: {}", count, films.size());
-        return films;
+        return filmService.getPopular(count);
     }
 }
--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -2,7 +2,6 @@ package ru.yandex.practicum.filmorate.controller;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.model.User;
@@ -12,55 +11,43 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/users")
-@Slf4j
 @RequiredArgsConstructor
 public class UserController {
     private final UserService userService;
 
     @GetMapping
     public List<User> getAll() {
-        log.info("GET /users");
         return userService.getAll();
     }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public User create(@Valid @RequestBody User user) {
-        log.info("==>POST /users {}", user);
-        User newUser = userService.save(user);
-        log.info("POST /user <== {}", newUser);
-        return newUser;
+        return userService.save(user);
     }
 
     @PutMapping
     public User update(@Valid @RequestBody User user) {
-        log.info("==>PUT /users {}", user);
-        User updatedUser = userService.update(user);
-        log.info("PUT /users <== {}", user);
-        return updatedUser;
+        return userService.update(user);
     }
 
     @PutMapping("/{id}/friends/{friendId}")
     public void addFriend(@PathVariable long id, @PathVariable long friendId) {
-       log.info("Add friend with id={} for User with id={}", friendId, id);
         userService.addFriend(id, friendId);
     }
 
     @DeleteMapping("/{id}/friends/{friendId}")
     public void deleteFriend(@PathVariable long id, @PathVariable long friendId) {
-        log.info("Delete friend with id={} from User with id={}", friendId, id);
         userService.deleteFriend(id, friendId);
     }
 
     @GetMapping("/{id}/friends")
     public  List<User> getFriends(@PathVariable long id) {
-        log.info("==> GET /friends for user with id={}", id);
         return userService.getFriends(id);
     }
 
     @GetMapping("/{id}/friends/common/{otherId}")
     public List<User> getCommonFriends(@PathVariable long id, @PathVariable long otherId) {
-        log.info("==> GET /common friends for users with id={} and {} <==", id, otherId);
         return userService.getCommonFriends(id, otherId);
     }
 }
